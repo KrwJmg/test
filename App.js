ead26561a02b9d9145300a42a470b92b8f483331
KrwJmg <flook_11323@hotmail.com>
2024-09-15 00:36:32 -0700

    add sup

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>yo nick</Text>
+      <Text>sup nick</Text>
       <StatusBar style="auto" />
     </View>
   );
